df98002809788969b0e67f87a26467cce339018e
Holden Gjuka <holdengjuka@gmail.com>
2022-10-15 13:53:43 -0700

    Instantiated darkmode theme and sample button

--- src/styles/theme/index.js
+++ src/styles/theme/index.js
@@ -1,2 +1,16 @@
 import { createTheme } from '@mui/material/styles'
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        disableRipple: true,
+      }
+    }
+  }
+})
+
+export default darkTheme

6240be91af2db8a4679d394b57d2052bf906c964
Holden Gjuka <holdengjuka@gmail.com>
2022-10-17 08:37:55 -0700

    Minor whitespace changes in appbarmobile, added browser width and height exports to theme/index.js

--- src/components/appbar/AppbarMobile.js
+++ src/components/appbar/AppbarMobile.js
@@ -5,10 +5,10 @@ import { MyList } from "../../styles/appbar";
 export default function AppbarMobile({ matches }) {
   return (
     <MyList type='row'>
-    <Button>Resume</Button>
-    <Button>About Me</Button>
-    <Button>Email Me</Button>
-    <Button>Upcoming Projects</Button>
-  </MyList>
+      <Button>Resume</Button>
+      <Button>About Me</Button>
+      <Button>Email Me</Button>
+      <Button>Upcoming Projects</Button>
+    </MyList>
   )
 }
--- src/styles/theme/index.js
+++ src/styles/theme/index.js
@@ -13,6 +13,29 @@ const darkTheme = createTheme({
   }
 })
 
-export const browserHeight = 600
+
+function getWidth() {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
+}
+
+function getHeight() {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  );
+}
+
+export const browserHeight = getHeight()
+
+export const browserWidth = getWidth()
 
 export default darkTheme

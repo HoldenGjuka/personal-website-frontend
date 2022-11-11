240e8674298210957a7c73b27d3d7b3822dd7425
Holden Gjuka <holdengjuka@gmail.com>
2022-11-10 22:01:52 -0800

    Fixed database_url value for debug

--- src/debug.js
+++ src/debug.js
@@ -2,10 +2,12 @@ const DEBUG = true
 
 let database_url
 
+//allows for a centralized database url source, and for the url to be changed
+// between development and debug setups
 if(DEBUG){
-  database_url = "127.0.0.1:8000"
+  database_url = "http://127.0.0.1:8000/"
 } else {
-  database_url = "127.0.0.1:8000"
+  database_url = "http://127.0.0.1:8000/"
 }
 
 export const db_url = database_url

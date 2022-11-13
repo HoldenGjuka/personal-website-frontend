371c6d71625da79f3bf1a7f7b7df25fcdca53243
Holden Gjuka <holdengjuka@gmail.com>
2022-11-12 20:12:41 -0800

    Changes to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 echo "Building app..."
 npm run build
 
+echo "Deleting currently deployed server build"
+sudo rm /var/www/holdengjuka.com/*
+
 echo "Deploying files to server..."
 sudo mv ./build/* /var/www/holdengjuka.com
 

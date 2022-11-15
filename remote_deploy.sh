d8c846dd619a64e931b970f032a6fe14f8297014
Holden Gjuka <holdengjuka@gmail.com>
2022-11-14 16:47:03 -0800

    remote_deploy.sh now works as intended.

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -2,6 +2,6 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to server..."
-sudo scp -r ./build/* holden@129.168.4.203:/var/www/holdengjuka.com/
+sudo scp -vr ./build/* holden@192.168.4.203:/var/www/holdengjuka.com/
 
 echo "Done"

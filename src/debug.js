12d9dcc9d39fbd87a57cbb303030b8087fe65951
Holden Gjuka <holdengjuka@gmail.com>
2022-12-06 12:48:57 -0800

    Fixed production database URL

--- src/debug.js
+++ src/debug.js
@@ -7,7 +7,7 @@ let database_url
 if(DEBUG){
   database_url = "http://127.0.0.1:7000/"
 } else {
-  database_url = "https://73.169.221.60/"
+  database_url = "https://holdengjuka.com/"
 }
 
 export const db_url = database_url

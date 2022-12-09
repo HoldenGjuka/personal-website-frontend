8da393f59dead71e481b8919531a9b124b11d3e1
Holden Gjuka <holdengjuka@gmail.com>
2022-12-09 11:08:27 -0800

    Text highlight added to header

--- src/styles/header/index.js
+++ src/styles/header/index.js
@@ -8,12 +8,9 @@ import theme, { Colors } from '../theme/index.js'
 
 //container
 export const HeaderContainer = styled(Box, Typography, theme)(() => ({
-  display: 'flex',
   height: '1/8',
   marginTop: 4,
   marginBottom:4,
-  justifyContent: 'center',
-  alignitems: 'center',
   [theme.breakpoints.down("md")]: {
     fontSize: '3em',
   },
@@ -34,5 +31,5 @@ const SpanBox = React.forwardRef(function SpanBox(props, ref) {
 });
 
 export const HighlightSpanBox = styled(SpanBox, Colors, theme)(() => ({
-  background: 'linear-gradient(180deg,rgba(255,255,255,0) 50%, #FFD0AE 50%)'
+  background: 'linear-gradient(180deg,rgba(255,255,255,0) 30%, ' + Colors.green + ' 30%)'
 }))

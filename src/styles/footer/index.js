065e694eccac8105bcecbc605f13609e2359a9ca
Holden Gjuka <holdengjuka@gmail.com>
2022-12-08 21:02:49 -0800

    Moved footer to right side and removed unecessary component folder and file

--- src/styles/footer/index.js
+++ src/styles/footer/index.js
@@ -1,10 +1,11 @@
 import { styled } from '@mui/material/styles'
-import { Box } from '@mui/system'
+import { Box, textAlign } from '@mui/system'
 import { Typography } from '@mui/material'
 
 //container
 export const FooterContainer = styled(Box, Typography)(() => ({
   margin: '10px',
   position: 'fixed',
-  bottom: '0'
+  bottom: '0',
+  right: '0'
 }))

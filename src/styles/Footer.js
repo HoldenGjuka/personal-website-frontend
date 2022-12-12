1738d9a260fa99334f4437c43a8123d796fbd8a2
Holden Gjuka <holdengjuka@gmail.com>
2022-12-11 18:04:28 -0800

    Fixed white sidebars on footer

--- src/styles/Footer.js
+++ src/styles/Footer.js
@@ -1,9 +1,12 @@
 import { styled } from '@mui/material/styles'
-import { Container } from '@mui/material'
+import { Box, Container } from '@mui/material'
 import { Colors } from './Theme'
 
 
-export const FooterContainer = styled(Container)(() => ({
+export const FooterContainer = styled(Box)(() => ({
   backgroundColor: Colors.pink,
   flex: ' 1 20px',
+  padding: 0,
+  disableGutters: true,
+  winWidth: '100%'
 }))
